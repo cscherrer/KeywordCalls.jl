7086c715b49025836b0b176c58fcd75c82ef54a7
Documenter.jl <documenter@juliadocs.github.io>
2021-04-26 17:49:15 +0000

    build based on 3557e17

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KeywordOrdering","category":"page"},{"location":"#KeywordOrdering","page":"Home","title":"KeywordOrdering","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KeywordOrdering.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KeywordOrdering]","category":"page"}]
+}
