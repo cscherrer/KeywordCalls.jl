a567a5c0980ea78c68ca3e1a64f9b3b4957ce216
Chad Scherrer <chad.scherrer@gmail.com>
2021-04-29 09:40:03 -0700

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using KeywordCalls
 using Test
 
+f(nt::NamedTuple{(:c,:b,:a)}) = nt.a^3 + nt.b^2 + nt.c
+@kwcall f(c,b,a)
+
 @testset "KeywordCalls.jl" begin
-    # Write your tests here.
+    @test @inferred f(1, 2, 3) == 32
+    @test @inferred f(a=1, b=2, c=3) == 8
+    @test @inferred f((a=1, b=2, c=3)) == 8
 end
