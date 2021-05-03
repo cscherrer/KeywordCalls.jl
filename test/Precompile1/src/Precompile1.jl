118871e59e297c62bacafcc8fc3f0fafb94fd29e
Rafael Fourquet <fourquet.rafael@gmail.com>
2021-05-03 10:47:33 +0200

    add test for cache with precompilation

---
+++ test/Precompile1/src/Precompile1.jl
@@ -0,0 +1,15 @@
+module Precompile1
+
+using KeywordCalls
+
+struct Foo{N,T}
+    nt::NamedTuple{N,T}
+end
+
+@kwcall Foo(a,b)
+
+Foo(nt::NamedTuple{(:a,:b), Tuple{A,B}}) where {A,B} = Foo{(:a,:b), Tuple{A,B}}(nt)
+
+export Foo
+
+end # module
