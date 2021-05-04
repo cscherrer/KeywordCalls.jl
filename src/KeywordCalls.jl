95568e6f80145d25a2486e95e574d52985cde4f9
Chad Scherrer <chad.scherrer@gmail.com>
2021-05-04 07:20:03 -0700

    `f, args... = rhs` pattern matching fails on Julia 1.4

--- src/KeywordCalls.jl
+++ src/KeywordCalls.jl
@@ -9,9 +9,16 @@ export @kwcall
 function _call_in_default_order end
 
 # Thanks to @simeonschaub for this implementation 
+"""
+@kwcall f(b,a,d)
+
+Declares that any call `f(::NamedTuple{N})` with `sort(N) == (:a,:b,:d)`
+should be dispatched to the method already defined on `f(::NamedTuple{(:b,:a,:d)})`
+"""
 macro kwcall(ex)
     @assert Meta.isexpr(ex, :call)
-    f, args... = ex.args
+    f = ex.args[1]
+    args = ex.args[2:end]
     f, args, sorted_args = esc(f), QuoteNode.(args), QuoteNode.(sort(args))
     return quote
         KeywordCalls._call_in_default_order(::typeof($f), nt::NamedTuple{($(sorted_args...),)}) = $f(NamedTuple{($(args...),)}(nt))
