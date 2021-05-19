1a4160522a0581de433995cd37e3fc23174ac9dc
Chad Scherrer <chad.scherrer@gmail.com>
2021-05-19 15:28:55 -0700

    docstring

--- src/KeywordCalls.jl
+++ src/KeywordCalls.jl
@@ -82,6 +82,16 @@ end
 
 export @kwalias
 
+"""
+    @kwalias f [
+        alpha => a
+        beta => b
+    ]
+
+declare that for the function `f`, we should consider `alpha` to be an alias for
+`a`, and `beta` to be an alias for `b`. In a call, the names will be mapped
+accordingly as a pre-processing step.
+"""
 macro kwalias(f, aliasmap)
     _kwalias(f, aliasmap)
 end
